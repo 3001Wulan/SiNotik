af77b708eac257e1f4198dfc06696aef4dfeb38e
Wulandari Yulianis <wulandariyulianis360@gmail.com>
2025-01-08 11:54:15 +0700

    landing page

---
+++ app/Views/Home.php
@@ -0,0 +1,31 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to SiNotik</title>
+    <link rel="stylesheet" href="Assets/css/home.css">
+
+</head>
+<body>
+    <header>
+    <img id="logo" src="assets/images/logo.png" alt="SiNotik Logo">
+    <p id="logo-text">SiNotik</p>
+    </header>
+
+    <nav>
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+    </nav>
+
+    <div class="container">
+        <h1>Welcome to SiNotik</h1>
+        <p>Optimalkan pencatatan rapat dengan cepat dan akurat. SiNotik menghadirkan kemudahan dalam dokumentasi dan pengelolaan hasil rapat, memastikan transparansi dan efisiensi di setiap pertemuan.</p>
+        <a href="login.php" class="login-btn">Login</a>
+    </div>
+</body>
+</html>
