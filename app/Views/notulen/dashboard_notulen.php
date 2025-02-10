b320ef5a8baf6d88b3b28ec8188063f80bc7e92a
Intan3005 <intansalma51@gmail.com>
2025-02-10 10:24:03 +0700

    dashboard_notulen

--- app/Views/notulen/dashboard_notulen.php
+++ app/Views/notulen/dashboard_notulen.php
@@ -22,7 +22,7 @@
                     </a>
                 </li>
                 <li class="dropdown">
-                    <a href="melihatpegawai" class="<?php echo ($current_page == 'melihat_pegawai') ? 'active notulensi-pegawai ' : 'inactive'; ?>">
+                    <a href="#" class="<?php echo ($current_page == 'melihat_pegawai') ? 'active notulensi-pegawai ' : 'inactive'; ?>">
                         <img src="<?php echo base_url('assets/images/codicon_book.png'); ?>" alt="Notulensi Icon" class="sidebar-icon">
                         Notulensi
                     </a>
@@ -37,6 +37,7 @@
                             <span>Buat Notulensi</span>
                         </a>
                     </div>
+                </li>
                 <li>
                     <a href="riwayatnotulen" class="<?php echo ($current_page == 'riwayat_notulensi') ? 'active riwayat-notulensi' : 'inactive'; ?>">
                         <img src="<?php echo base_url('assets/images/riwayatnotulensi.png'); ?>" alt="Riwayat Notulensi Icon" class="sidebar-icon">
